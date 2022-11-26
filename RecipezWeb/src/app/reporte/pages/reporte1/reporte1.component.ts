a880b809b0259a0b4e683df5c607bff3eb38b246
ricardo3107 <111294442+ricardo3107@users.noreply.github.com>
2022-11-26 07:03:47 -0500

    reporte
    .

--- RecipezWeb/src/app/reporte/pages/reporte1/reporte1.component.ts
+++ RecipezWeb/src/app/reporte/pages/reporte1/reporte1.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Reporte1Component implements OnInit {
 
+  lineChartData={
+    labels:["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+    datasets:[
+      {
+        data:[0,89,34,45,54,28,74,93,80,90,70,75],
+        label:'Membresias compradas'
+      }
+    ]
+  }
+
   constructor() { }
 
   ngOnInit(): void {
